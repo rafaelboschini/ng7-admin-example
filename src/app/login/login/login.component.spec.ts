c097fa15185853a8a7fa34482cf21fff2566cc61
rboschini <rafaelboschini@gmail.com>
2019-08-03 04:19:03 -0300

    create login test script

--- src/app/login/login/login.component.spec.ts
+++ src/app/login/login/login.component.spec.ts
@@ -22,4 +22,22 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create form and labels', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    /** testing form and fields */
+    expect(compiled.querySelector('input[type="email"]')).toBeTruthy();
+    expect(compiled.querySelector('input[type="password"]')).toBeTruthy();
+    expect(compiled.querySelector('label[for="username"]')).toBeTruthy();
+    
+    /** Testing labels */
+    expect(compiled.querySelector('label[for="username"]').textContent).toBe('E-mail');
+    expect(compiled.querySelector('label[for="password"]').textContent).toBe('Password');
+
+    /** testing if have submit button */
+    expect(compiled.querySelector('button[type="submit"]')).toBeTruthy();
+  });
 });
