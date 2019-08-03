d1265d667d85c4976d571f069a53e38b53f49c62
rboschini <rafaelboschini@gmail.com>
2019-08-03 15:04:50 -0300

    add user and profile models

---
+++ src/app/_models/user-profile.ts
@@ -0,0 +1,4 @@
+export class UserProfile {
+    id: number;
+    name: string;
+}
---
+++ src/app/_models/user.ts
@@ -0,0 +1,11 @@
+import { UserProfile } from './user-profile';
+
+export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    document: string;
+    profile: UserProfile;
+    token: string;
+}
