ddbbbdc372d71898e717906c3376fdfbcdc35f05
rboschini <rafaelboschini@gmail.com>
2019-08-26 12:03:55 -0300

    user form front structure

--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+import { UserService } from 'src/app/_services/user/user.service';
+import { User, UserProfile } from 'src/app/_models';
 
 @Component({
   selector: 'app-user-form',
@@ -6,10 +12,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+  userForm: FormGroup;
+  submitted = false;
+  selectedUser: User = new User;
 
-  constructor() { }
+  profiles: UserProfile[] = [
+    {id: 0, name: 'Admin'},
+    {id: 1, name: 'Viewer'}
+  ];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      username: ['', Validators.required],
+      profile: ['', Validators.required],
+      password: ['', [Validators.required]],
+      cpf: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    this.submitted = true;
+
+    console.log("userForm invalid", this.userForm.invalid);
+
+    if (this.userForm.invalid) {
+      return;
+    }
+
+    console.log("Chegou");
+
+    if (!this.selectedUser.id) {
+      /** Register new user */
+      this.userService.register(this.userForm.value)
+              .pipe(first())
+              .subscribe(this.successHandler, this.errHandler);
+    } else {
+      /** Editing existing user */
+      this.userService.update(this.selectedUser)
+              .pipe(first())
+              .subscribe(this.successHandler, this.errHandler);
+    }
+  }
+
+  get f() { return this.userForm.controls; }
+
+  errHandler(error) {
+    this.submitted = false;
+  }
+
+  successHandler() {
+    this.router.navigate(['/user']);
+  }
+
+  changeProfileValue(e) {
+    this.f.profile.setValue(e.target.value, {
+      onlySelf: true
+    });
+  }
+  
 }
